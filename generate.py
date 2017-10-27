ebf3ee2b729e23d8d6e9fa3822876a172dceff64
Jacob Kahn <jacobkahn1@gmail.com>
2017-10-26 23:31:20 -0400

    deep learning clients and structured perceptron integration
    note that structured perceptron currently does worse than random guessing

--- generate.py
+++ generate.py
@@ -1,15 +1,32 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
-from keras import backend as K
-from keras import losses
-import tensorflow as tf
+from perceptron import Constraint
 import itertools
 
-def test_constraint(y1, y2):
-    # Write constraint function - add constraints as bool exprs
-    # returns true if should pass constraint
-    return (y1 * y2 == 1)
+
+class RandomConstraint(Constraint):
+    """
+    Sample constriant that checks
+    """
+    def __init__(self, index_array, poss_assign):
+        """
+        index_array: an array of indices for which the constraints are relevant
+                     in the input y data
+        poss_assign: the possible valid structured outputs for some function on
+                     the data
+        """
+        self.index_arry = index_array
+        self.assignments = poss_assign
+
+
+    def evaluate(self, y):
+        """
+        evaluate the constraint on a specified data point y
+        """
+        const_projection = np.zeros(len(self.index_array))
+        for i in range(len(self.index_array)):
+            const_projection[i] = c[self.index_array[i]]
+        return const_projection in self.assignments
+
 
 def generate_subsets(seq_length, num_subsets, subset_size=2):
     subsets = set()
@@ -54,10 +71,13 @@ def generate_pairwise_dependent(seq_length, num_examples, num_constraints):
     for _ in range(num_examples):
         weights.append(np.random.random_sample((seq_length,))*2-1)
     constraints = generate_subsets(seq_length, num_constraints, 2)
-    print(constraints)
+    # constraints compatible with perceptron implementation
+    good_constraints = []
+    for constraint in constraints:
+        good_constraints.append(RandomConstraint(constraint, [[0, 0], [0, 1],[1, 0]]))
+
     outputs = []
     count = 0
-    # for each example compute the argmax over constraint space
     for j in range(num_examples):
         f_vec = []
         for i in range(seq_length):
@@ -66,20 +86,14 @@ def generate_pairwise_dependent(seq_length, num_examples, num_constraints):
         y = None
         max_score = -np.inf
         binary_arrs = _binary_arrs(seq_length)
-        # go through all structure
         for a in binary_arrs:
-            # TODO: viterbi
-            # check if cosntraint is violated. also only consider
-            # constraints that beat the max score
             if np.dot(2*np.array(a)-1, f_vec) > max_score:
                 invalid_flag = False
                 for c in constraints:
                     c = list(c)
-                    # constraint line
-                    if test_constraint(a[c[0]], a[c[1]]):
+                    if a[c[0]]*a[c[1]] == 1:
                         invalid_flag = True
                         break
-                    # don't consider if violated
                 if not invalid_flag:
                     y = np.array(a)
                     max_score = np.dot(2*np.array(a)-1, f_vec)
@@ -87,14 +101,11 @@ def generate_pairwise_dependent(seq_length, num_examples, num_constraints):
         for i in range(seq_length):
             if f_vec[i] >= 0:
                 y_unconstrained[i] = 1
-        # y is the argmax over the constraint space
-        # y_unconstrainted is "" over unconstrained space
-        if np.dot(y - y_unconstrained, y - y_unconstrained) != 0:
+        if np.dot(y-y_unconstrained, y-y_unconstrained) != 0:
             count += 1
         outputs.append(y)
     print('Kappa:', float(count)/num_examples)
-    return np.array(inputs), np.array(outputs)
-
+    return np.array(inputs), np.array(outputs), good_constraints
 
 def separate_train_test(inputs, outputs, test_frac=0.2):
     n = len(inputs)
--- model.py
+++ model.py
@@ -4,8 +4,43 @@ from keras.layers import Dense
 from keras import backend as K
 from keras import losses
 import tensorflow as tf
-
+import perceptron
 from generate import *
+
+"""
+Where the learning happens
+"""
+class LearningClient(object):
+    def __init__(self, train, test, seq_length):
+        self.train = train
+        self.test = test
+        self.seq_length = seq_length
+
+
+class DeepLearningClient(LearningClient):
+    def run(self):
+        # do a deep learning
+        model = Sequential()
+        model.add(Dense(self.seq_length, activation='relu', input_dim=self.seq_length))
+        model.add(Dense(self.seq_length, activation='sigmoid', input_dim=self.seq_length))
+        model.compile(optimizer='sgd', loss="binary_crossentropy", metrics=['accuracy'])
+        model.fit(self.train[0], self.train[1], epochs=1000, batch_size=32)
+        score = model.evaluate(self.test[0], self.test[1], batch_size=128)
+        return score
+
+
+
+class PerceptronClient(LearningClient):
+    def run(self, constraints):
+        # do a structured_perceptron
+        structured_perceptron = perceptron.Perceptron(20, self.seq_length)
+        for constraint in constraints:
+            structured_perceptron.add_constraints(constraint)
+        train_result = structured_perceptron.train(self.train[0], self.train[1])
+        test_result = structured_perceptron.test(self.test[0], self.test[1])
+        return (train_result, test_result)
+
+
 
 def proxy_bce(y_true, y_pred):
     """_epsilon = tf.convert_to_tensor(K.epsilon())
@@ -17,12 +52,16 @@ def proxy_bce(y_true, y_pred):
     return K.mean(K.binary_crossentropy(y_pred, y_true), axis=-1)
 
 def run(seq_length, num_examples, epochs=10, num_constraints=0):
-    [inputs, outputs] = generate_pairwise_dependent(seq_length, num_examples, num_constraints)
+    [inputs, outputs, constraints] = generate_pairwise_dependent(seq_length, num_examples, num_constraints)
     [train, test] = separate_train_test(inputs, outputs)
-    model = Sequential()
-    model.add(Dense(seq_length, activation='relu', input_dim=seq_length))
-    model.add(Dense(seq_length, activation='sigmoid', input_dim=seq_length))
-    model.compile(optimizer='sgd', loss="binary_crossentropy", metrics=['accuracy'])
-    model.fit(train[0], train[1], epochs=1000, batch_size=32)
-    score = model.evaluate(test[0], test[1], batch_size=128)
-    return score
+
+    dlresult = DeepLearningClient(train, test, seq_length).run()
+    perceptron_client = PerceptronClient(train, test, seq_length)
+    [p_train, p_test] = perceptron_client.run(constraints)
+
+    return (dlresult, p_test)
+
+
+# the main function
+if __name__ == "__main__":
+    print run(5, 1000)
