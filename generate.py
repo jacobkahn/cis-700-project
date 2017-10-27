65bdc6a25e909f3c70b66af70aeac11b2d825d58
Jacob Kahn <jacobkahn1@gmail.com>
2017-10-26 22:15:37 -0400

    hmm start

--- generate.py
+++ generate.py
@@ -6,6 +6,11 @@ from keras import losses
 import tensorflow as tf
 import itertools
 
+def test_constraint(y1, y2):
+    # Write constraint function - add constraints as bool exprs
+    # returns true if should pass constraint
+    return (y1 * y2 == 1)
+
 def generate_subsets(seq_length, num_subsets, subset_size=2):
     subsets = set()
     c = 0
@@ -52,6 +57,7 @@ def generate_pairwise_dependent(seq_length, num_examples, num_constraints):
     print(constraints)
     outputs = []
     count = 0
+    # for each example compute the argmax over constraint space
     for j in range(num_examples):
         f_vec = []
         for i in range(seq_length):
@@ -60,14 +66,20 @@ def generate_pairwise_dependent(seq_length, num_examples, num_constraints):
         y = None
         max_score = -np.inf
         binary_arrs = _binary_arrs(seq_length)
+        # go through all structure
         for a in binary_arrs:
+            # TODO: viterbi
+            # check if cosntraint is violated. also only consider
+            # constraints that beat the max score
             if np.dot(2*np.array(a)-1, f_vec) > max_score:
                 invalid_flag = False
                 for c in constraints:
                     c = list(c)
-                    if a[c[0]]*a[c[1]] == 1:
+                    # constraint line
+                    if test_constraint(a[c[0]], a[c[1]]):
                         invalid_flag = True
                         break
+                    # don't consider if violated
                 if not invalid_flag:
                     y = np.array(a)
                     max_score = np.dot(2*np.array(a)-1, f_vec)
@@ -75,11 +87,14 @@ def generate_pairwise_dependent(seq_length, num_examples, num_constraints):
         for i in range(seq_length):
             if f_vec[i] >= 0:
                 y_unconstrained[i] = 1
-        if np.dot(y-y_unconstrained, y-y_unconstrained) != 0:
+        # y is the argmax over the constraint space
+        # y_unconstrainted is "" over unconstrained space
+        if np.dot(y - y_unconstrained, y - y_unconstrained) != 0:
             count += 1
         outputs.append(y)
     print('Kappa:', float(count)/num_examples)
     return np.array(inputs), np.array(outputs)
+
 
 def separate_train_test(inputs, outputs, test_frac=0.2):
     n = len(inputs)
