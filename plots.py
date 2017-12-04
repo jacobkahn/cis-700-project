f6e253d984d4b77a555ef5011dc3bd815781dfea
Jacob Kahn <jacokahn@seas.upenn.edu>
2017-12-04 12:58:23 -0500

    Add plots for new data

--- plots.py
+++ plots.py
@@ -19,15 +19,15 @@ def generate_learning_curve(seq_length, num_constraints):
 
 def learning_curve_csv(csv_name):
     data = pd.read_csv(csv_name)
-    for c, f  in zip([0, 5, 10, 15], [111, 112, 113, 114]):
+    for c, f  in zip([0, 5, 10, 20, 30], [110, 111, 112, 113, 114]):
         plt.figure(f)
-        plt.plot(data[data.constraints == c]['training_size'], data[data.constraints == c]['ffn'], 'b-', label='Feedforward Network')
-        plt.plot(data[data.constraints == c]['training_size'], data[data.constraints == c]['perceptron'], '-', color='violet', label='Structured Perceptron')
-        plt.plot(data[data.constraints == c]['training_size'], data[data.constraints == c]['local'], '-', color='orange', label='Local')
+        plt.plot(data[data.num_constraints == c][data.seq_length == 20]['num_examples'], data[data.num_constraints == c][data.seq_length == 20]['ffn'], 'b-', label='Feedforward Network')
+        plt.plot(data[data.num_constraints == c][data.seq_length == 20]['num_examples'], data[data.num_constraints == c][data.seq_length == 20]['perceptron'], '-', color='violet', label='Structured Perceptron')
+        plt.plot(data[data.num_constraints == c][data.seq_length == 20]['num_examples'], data[data.num_constraints == c][data.seq_length == 20]['localclassifier'], '-', color='orange', label='Local')
         plt.legend()
-        plt.xlim(0, 2100)
+        plt.xlim(0, 5000)
         plt.ylim(0.5, 1)
         plt.xlabel("Training Set Size")
         plt.ylabel("Test Set Accuracy")
         plt.title("Accuracy vs. Training Set Size - " + str(c) + " Constraints")
-    plt.show(block=False)
+    plt.show(block=True)
